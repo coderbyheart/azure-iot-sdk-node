43ae0467979b373b90eb9602bda0a1aa9ed38980
Anthony V. Ercolano <toercola@microsoft.com>
2019-10-03 15:37:26 -0700

    try node 10

--- provisioning/e2e/_service_create_delete.js
+++ provisioning/e2e/_service_create_delete.js
@@ -144,152 +144,160 @@ describe('Provisioning Service Client: CRUD operations', function () {
       enrollmentObject: symmetricKeyEnrollmentGroup
     }
   ];
-  testSpecification.forEach(function(testConfiguration) {
-    describe('#Create', function() {
+  testSpecification.forEach((testConfiguration) => {
+    describe('#Create', () => {
       let enrollmentForCreateTest = {};
-      after(function(done) {
+      after((callbackForAfterOfCreate) => {
         debug('In after of #Create: before get: enrollment record etag: ' + enrollmentForCreateTest.etag);
         debug('In after of #Create: before get: entire enrollment record' + JSON.stringify(enrollmentForCreateTest));
-        testConfiguration.getFunction(enrollmentForCreateTest[testConfiguration.idPropertyName], function (err, getResult) {
-          if (err) {
-            debug(err);
+        testConfiguration.getFunction(enrollmentForCreateTest[testConfiguration.idPropertyName], (getErr, getResult) => {
+          if (getErr) {
+            debug(getErr);
           } else {
             debug('In after of #Create: after get: enrollment record etag: ' + enrollmentForCreateTest.etag);
             debug('In after of #Create: just before delete: getResult - ' + JSON.stringify(getResult));
-            testConfiguration.deleteFunction(getResult, function(err) {
+            let createContextUuid = uuid.v4();
+            testConfiguration.deleteFunction(getResult, (deleteErr) => {
+              debug('In after of #Create: just AFTER delete invocation: contextId is: ' + createContextUuid);
               debug('In after of #Create: just AFTER delete invocation: getResult - ' + JSON.stringify(getResult));
-              if (err) {
-                debug(err);
+              if (deleteErr) {
+                debug(deleteErr);
               }
-              assert.isNull(err, 'Non null response from the delete AFTER create.');
-              done();
+              assert.isNull(deleteErr, 'Non null response from the delete AFTER create.');
+              callbackForAfterOfCreate();
             });
           }
         });
       });
-      it(testConfiguration.testDescription, function(callback) {
+      it(testConfiguration.testDescription, (callbackForItOfCreate) => {
         debug('In #Create IT with enrollment object: ' + JSON.stringify(testConfiguration.enrollmentObject));
-        testConfiguration.createFunction(testConfiguration.enrollmentObject, function(err, returnedEnrollment) {
-          if (err) {
-            debug(err);
+        testConfiguration.createFunction(testConfiguration.enrollmentObject, (createErr, returnedEnrollment) => {
+          if (createErr) {
+            debug(createErr);
           }
           debug('In #Create IT after Create - returned enrollment is: ' + JSON.stringify(returnedEnrollment));
-          assert.isNull(err,'Should be no error from the create');
+          assert.isNull(createErr,'Should be no error from the create');
           enrollmentForCreateTest = returnedEnrollment;
-          callback();
+          callbackForItOfCreate();
         });
       });
     });
   });
 
-  testSpecification.forEach(function(testConfiguration) {
-    describe('#Delete', function() {
+  testSpecification.forEach((testConfiguration) => {
+    describe('#Delete', () => {
       let enrollmentForDeleteTest = {};
-      before(function(done) {
+      before((callbackForBeforeOfDelete) => {
         debug('In before for #delete - config enrollmentObject is: ' + JSON.stringify(testConfiguration.enrollmentObject));
-        testConfiguration.createFunction(testConfiguration.enrollmentObject, function(err, returnedEnrollment) {
-          if (err) {
-            debug(err);
+        testConfiguration.createFunction(testConfiguration.enrollmentObject, (createErr, returnedEnrollment) => {
+          if (createErr) {
+            debug(createErr);
           }
-          assert.isNull(err, 'Should be no error from the BEFORE create');
+          assert.isNull(createErr, 'Should be no error from the BEFORE create');
           debug('In before for #delete - returnedEnrollment is: ' + JSON.stringify(returnedEnrollment));
           enrollmentForDeleteTest = returnedEnrollment;
-          done();
+          callbackForBeforeOfDelete();
         });
       });
-      it(testConfiguration.testDescription, function(callback) {
+      it(testConfiguration.testDescription, (callbackForItOfDelete) => {
         debug('In IT for #Delete - enrollmentForDeleteTest is: ' + JSON.stringify(enrollmentForDeleteTest));
-        testConfiguration.deleteFunction(enrollmentForDeleteTest[testConfiguration.idPropertyName], enrollmentForDeleteTest.etag, function(err) {
+        let deleteContextUuid = uuid.v4();
+        testConfiguration.deleteFunction(enrollmentForDeleteTest[testConfiguration.idPropertyName], enrollmentForDeleteTest.etag, (deleteError) => {
+          debug('In IT for #Delete - after delete invocation - contextUuid is: ' + deleteContextUuid);
           debug('In IT for #Delete - after delete invocation - enrollmentForDeleteTest is: ' + JSON.stringify(enrollmentForDeleteTest));
-          if (err) {
-            debug(err);
+          if (deleteError) {
+            debug(deleteError);
           }
-          assert.isNull(err, 'Non null response from the delete.');
-          callback();
+          assert.isNull(deleteError, 'Non null response from the delete.');
+          callbackForItOfDelete();
         });
       });
     });
   });
 
-  testSpecification.forEach(function(testConfiguration) {
-    describe('#Update', function() {
+  testSpecification.forEach((testConfiguration) => {
+    describe('#Update', () => {
       let enrollmentReturnedFromUpdate = {};
       let enrollmentToUpdate = {};
-      before(function(done) {
+      before((callbackForAfterOfUpdate) => {
         debug('in before of #Update - enrollment to create is: ' + JSON.stringify(testConfiguration.enrollmentObject));
-        testConfiguration.createFunction(testConfiguration.enrollmentObject, function(err, returnedEnrollment) {
-          if (err) {
-            debug(err);
+        testConfiguration.createFunction(testConfiguration.enrollmentObject, (createErr, returnedEnrollment) => {
+          if (createErr) {
+            debug(createErr);
           }
-          assert.isNull(err, 'Should be no error from the create');
+          assert.isNull(createErr, 'Should be no error from the create');
           debug('In before of #Update - returnedEnrollment is: ' + JSON.stringify(returnedEnrollment));
           enrollmentToUpdate = returnedEnrollment;
-          done();
+          callbackForAfterOfUpdate();
         });
       });
-      after(function(done) {
+      after((callbackForAfterOfUpdate) => {
         debug('In after of #Update - enrollmentReturnedFromUpdate is: ' + JSON.stringify(enrollmentReturnedFromUpdate));
-        testConfiguration.deleteFunction(enrollmentReturnedFromUpdate, function(err) {
+        let updateContextUuid = uuid.v4();
+        testConfiguration.deleteFunction(enrollmentReturnedFromUpdate, (deleteErr) => {
+          debug('In after for #update after delete invocation - context uuid is: ' + updateContextUuid);
           debug('In after for #update after delete invocation for enrollmentReturnedFromUpdate: ' + JSON.stringify(enrollmentReturnedFromUpdate));
-          if (err) {
-            debug(err);
+          if (deleteErr) {
+            debug(deleteErr);
           }
-          assert.isNull(err, 'Non null response from the delete AFTER create.');
-          done();
+          assert.isNull(deleteErr, 'Non null response from the delete AFTER create.');
+          callbackForAfterOfUpdate();
         });
       });
-      it(testConfiguration.testDescription, function(callback) {
+      it(testConfiguration.testDescription, (callbackForItOfUpdate) => {
         enrollmentToUpdate.provisioningStatus = 'disabled';
         debug('In IT test for #Update - enrollmentToUpdate is: ' + JSON.stringify(enrollmentToUpdate));
-        testConfiguration.updateFunction(enrollmentToUpdate, function(err, updatedEnrollment) {
-          if (err) {
-            debug(err);
+        testConfiguration.updateFunction(enrollmentToUpdate, (updateErr, updatedEnrollment) => {
+          if (updateErr) {
+            debug(updateErr);
           }
-          assert.isNull(err);
+          assert.isNull(updateErr);
           assert.equal(updatedEnrollment.provisioningStatus, 'disabled', 'provisioning state not disabled');
           debug('In IT test for #Update - updatedEnrollment is: ' + JSON.stringify(updatedEnrollment));
           enrollmentReturnedFromUpdate = updatedEnrollment;
-          callback();
+          callbackForItOfUpdate();
         });
       });
     });
   });
 
-  testSpecification.forEach(function(testConfiguration) {
-    describe('#getAttestationMechanism', function () {
+  testSpecification.forEach((testConfiguration) => {
+    describe('#getAttestationMechanism', () => {
       let enrollmentToVerify = {};
-      before(function(done) {
+      before((callbackForBeforeOfGetAttestation) => {
         debug('in before for #getAttest - enrollmentObject is: ' + JSON.stringify(testConfiguration.enrollmentObject));
-        testConfiguration.createFunction(testConfiguration.enrollmentObject, function(err, returnedEnrollment) {
-          if (err) {
-            debug(err);
+        testConfiguration.createFunction(testConfiguration.enrollmentObject, (createError, returnedEnrollment) => {
+          if (createError) {
+            debug(createError);
           }
-          assert.isNull(err, 'Should be no error from the create');
+          assert.isNull(createError, 'Should be no error from the create');
           debug('In before for #getAttest - returnedEnrollment is: ' + JSON.stringify(returnedEnrollment));
           enrollmentToVerify = returnedEnrollment;
-          done();
+          callbackForBeforeOfGetAttestation();
         });
       });
-      after(function(done) {
+      after((callbackForAfterOfGetAttestation) => {
         debug('in after of #getAttest - enrollmentToVerify to be deleted is: ' + JSON.stringify(enrollmentToVerify));
-        testConfiguration.deleteFunction(enrollmentToVerify, function(err) {
+        let getAttestContextUuid = uuid.v4();
+        testConfiguration.deleteFunction(enrollmentToVerify, (deleteError) => {
+          debug('in after of #getAttest AFTER delete invocation - getAttestContextUuid is: ' + getAttestContextUuid);
           debug('in after of #getAttest AFTER delete invocation - enrollmentToVerify deleted is: ' + JSON.stringify(enrollmentToVerify));
-          if (err) {
-            debug(err);
+          if (deleteError) {
+            debug(deleteError);
           }
-          assert.isNull(err, 'Non null response from the delete AFTER create.');
-          done();
+          assert.isNull(deleteError, 'Non null response from the delete AFTER create.');
+          callbackForAfterOfGetAttestation();
         });
       });
-      it(testConfiguration.testDescription, function(done) {
+      it(testConfiguration.testDescription, (callbackForItOfGetAttestation) => {
         debug('In IT test of getAttest - enrollmentToVerify is: ' + JSON.stringify(enrollmentToVerify));
-        testConfiguration.getAttestationMechanismFunction(enrollmentToVerify[testConfiguration.idPropertyName], function (err, attestationMechanism) {
-          if (err) {
-            debug(err);
+        testConfiguration.getAttestationMechanismFunction(enrollmentToVerify[testConfiguration.idPropertyName], (getAttestErr, attestationMechanism) => {
+          if (getAttestErr) {
+            debug(getAttestErr);
           }
-          assert.isNull(err);
+          assert.isNull(getAttestErr);
           assert.strictEqual(testConfiguration.enrollmentObject.attestation.type, attestationMechanism.type);
-          done();
+          callbackForItOfGetAttestation();
         });
       });
     });
